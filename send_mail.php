e91b82208ea872e00424b0142d69a4072356de22
Maurizio Lorenzetti <mlorenze23@gmail.com>
2019-04-14 21:16:08 -0500

    php mail fix trial 2

--- send_mail.php
+++ send_mail.php
@@ -1,22 +1,18 @@
 <?php
-if (isset($_POST['submit'])) {
     $name = $_POST['customer_name'];
     $emailfrom = $_POST['customer_email'];
-    $subject = "Inquiry"
+    $subject = "Email TEST"
     $emailfrom = $_POST['customer_email'];
     $tel = $_POST['customer_tel'];
     $message = $_POST['customer_message'];
 
     $emailto = "maurizio.23@hotmail.com";
-    $headers = "From: ".$emailfrom;
+    $headers = "From: lorenzetti23@aol.com" . "\r\n".;
     $txt = "You have received an email from ".$name.".\n\n".$message;
 
-    mail($emailto, $subject, $txt, $headers);
-    headers("Location: /send_email.php");
-}
+    mail($emailto, $subject, $message, $headers);
+
 
 
 ?>
 
-<h1>Thanks for contacting us! Will get back to you soon </h1>
-<button onclick="window.location.href = 'https://mauriziol23.github.io/DopeMeal/';">Back to site</button>
